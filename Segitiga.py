d0709d2c36a9e8681173064840d38c163446a234
muhahdan032-wq <muhahdan032@gmail.com>
2025-11-09 19:23:27 +0800

    Create Segitiga.py

---
+++ Segitiga.py
@@ -0,0 +1,8 @@
+#nama : Muhammad Ahdan 
+#kelas : sistem informasi B
+#nim : D0425317
+
+alas_segitiga = int(input("masukan alas segitiga :"))
+tinggi_segitiga = int(input("masukan tinggi segitiga : "))
+luas = alas_segitiga * tinggi_segitiga
+print(f"alas : {alas_segitiga} x {tinggi_segitiga} : {luas}")

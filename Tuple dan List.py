1e8760a2aad2faed332650b147cdfadd35cc4781
muhahdan032-wq <muhahdan032@gmail.com>
2025-11-26 23:29:38 +0800

    Create Tuple dan List.py

---
+++ Tuple dan List.py
@@ -0,0 +1,36 @@
+# Muhammad Ahdan 
+# D0425317
+# Kelas B Sistem Informasi 
+
+
+#tuple
+
+
+
+profesi = ("guru", "dosen", "dokter") #tuple tidak bisa di ubah
+
+print("isi tuple: ", profesi)
+
+print("Elemen ke-1:", profesi[0])
+
+# profesi[1] = "dosen"  # Ini akan ERROR karena tuple tidak bisa diubah
+
+
+
+
+
+#List
+
+profesi = ["guru", "dosen", "dokter"] #Bisa diubah
+
+print("Sebelum diubah:", profesi)
+
+
+
+profesi[1] = "dokter"     # mengubah isi
+
+profesi.append("polisi")  # menambah data
+
+
+
+print("Setelah diubah:", profesi)

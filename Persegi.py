1de900480fd28a4f45e5f8911e315d0583b9fa4a
muhahdan032-wq <muhahdan032@gmail.com>
2025-11-27 08:36:00 +0800

    Create Persegi.py

---
+++ Persegi.py
@@ -0,0 +1,10 @@
+# Nama : Muhammad Ahdan
+# nim : D0425317
+# luas PERSEGI
+
+sisi = int(input("Masukkan sisi pertama: "))
+sisikedua = int(input("Masukkan sisi kedua: "))
+
+luas = int(sisi * sisikedua)
+
+print('luas persegi adalah: ' , luas)
